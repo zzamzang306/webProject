9f6e1819f8542d7567abf6a911cb3d09a0ac187c
jreyesco16 <ladiesman28170@gmail.com>
2021-02-25 00:18:23 -0600

    added to search page

---
+++ frontend/src/search/Dropdown.js
@@ -0,0 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import'bootstrap/dist/js/bootstrap.bundle.min';
+
+function Dropdown_Country() {
+    return (
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                    Dropdown button
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Dropdown_Country
--- frontend/src/search/Search.js
+++ frontend/src/search/Search.js
@@ -1,4 +1,9 @@
 import "./Search.css"
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import Dropdown from './Dropdown'
+import Dropdown from "./Dropdown"
+import { Form } from 'react-bootstrap';
+
 
 function Search() {
     return (
@@ -7,16 +12,24 @@ function Search() {
                 <title>Search</title>
                 <meta charset="utf-8"/>
                 <link rel="stylesheet"/>
+                <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/cerulean/bootstrap.min.css" rel="stylesheet" integrity="sha384-zF4BRsG/fLiTGfR9QL82DrilZxrwgY/+du4p/c7J72zZj+FLYq4zY00RylP9ZjiT" crossorigin="anonymous"/>
             </head>
 
             <body>
+
                 <head>Search</head>
 
                 <main>
+                    <h1 style={{textAlign: "center", marginTop:"15%" }}>Search Dates:</h1>
                     <form class="address">
-                        Country<br/><input type="text" name="country" id="country"/><br/>
-                        Date<br/><input type="text" name="date" id="date"/><br/>
-                        Place<br/><input type="text" name="place" id="place"/>
+                        Country <Dropdown/><br/>
+                        {/* <input type="text" name="country" class="country"/> <br/> */}
+                        {/* <Dropdown/> */}
+                        Date <Form.Control className="meet-date" type="date" name="dob" placeholder="Date of Birth" style={{margin:"0 auto"}}/> <br/>
+                        {/* <input type= "text" class= "form-control" data-provide="datepicker"></input><br/> */}
+                        {/* <input type="text" name="date" id="date"/><br/> */}
+                        Place <Dropdown/><br/>
+                        {/* <input type="text" name="place" id="place"/> */}
                     </form>
                 </main>
         
